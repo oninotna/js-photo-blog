9af03775310722301ec0a00b4cd366d6b17ecac9
oninotna <antoninomusarratubbi@gmail.com>
2025-04-24 16:57:04 +0200

    added ajax call and push element in html

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-// const cards = document.querySelector('.cards');
-// cards.innerHTML += `
-//     <div class="col-12 col-md-6 col-lg-4 mb-4">
-//         <div class="my-card">
-//             <img src="https://picsum.photos/200/700"/>
-//             <div class="description-img-my-card edu-tas-beginner">Questa e' una prova layout per le card</div>
-//         </div>
-//     </div>
-// `;
+apiUri = 'https://lanciweb.github.io/demo/api/pictures/';
+const cards = document.querySelector(".cards-row");
+
+axios.get(apiUri)
+.then(response => {
+    imgDataList =response.data;
+
+    imgDataList.forEach(element => {
+        const {date, title, id, url} = element;
+        cards.innerHTML += 
+        `<div class="col-12 col-md-6 col-lg-4 mb-4 card-col">
+            <div class="my-card">
+                <div class="img-my-card" style="background-image: url(${url});"></div>
+                <div class="date">${date}</div>
+                <div class="description-img-my-card edu-tas-beginner">${title}</div>
+            </div>
+        </div>`
+    })
+    
+});
