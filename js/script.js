767e9275fab1c7e3a433039c5902652947f78e93
oninotna <antoninomusarratubbi@gmail.com>
2025-04-24 16:22:36 +0200

    cards structure modified

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// const cards = document.querySelector('.cards');
+// cards.innerHTML += `
+//     <div class="col-12 col-md-6 col-lg-4 mb-4">
+//         <div class="my-card">
+//             <img src="https://picsum.photos/200/700"/>
+//             <div class="description-img-my-card edu-tas-beginner">Questa e' una prova layout per le card</div>
+//         </div>
+//     </div>
+// `;
