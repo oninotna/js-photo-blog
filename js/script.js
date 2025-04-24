09e6f33a93b934d7495d5250b9c6545759fc0720
oninotna <antoninomusarratubbi@gmail.com>
2025-04-24 17:29:10 +0200

    added pin-card

--- js/script.js
+++ js/script.js
@@ -10,11 +10,11 @@ axios.get(apiUri)
         cards.innerHTML += 
         `<div class="col-12 col-md-6 col-lg-4 mb-4 card-col">
             <div class="my-card">
+                <img class="pin-card" src="./img/pin.svg">
                 <div class="img-my-card" style="background-image: url(${url});"></div>
                 <div class="date">${date}</div>
                 <div class="description-img-my-card edu-tas-beginner">${title}</div>
-            </div>
+            </img>
         </div>`
-    })
-    
+    });  
 });
